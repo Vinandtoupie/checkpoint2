2fdae1dadacdd7173a7e4a80a77dca81495907e7
vincent <vincentcharlettepro@gmail.com>
2022-03-18 09:58:19 +0100

    starting1

--- checkpoint2/src/App.js
+++ checkpoint2/src/App.js
@@ -1,9 +1,11 @@
+import Header from "./components/Header";
 
 
 function App() {
+  const name = "REACT GAMES"
   return (
     <div className="App">
-
+      <Header name={name} />
     </div>
   );
 }
